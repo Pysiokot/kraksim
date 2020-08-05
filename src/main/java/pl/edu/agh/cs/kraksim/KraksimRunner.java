b985098039de16345963cac476c199f8cb40d4f1
Pysiokot <bszczypka@nano-games.com>
2020-08-05 16:56:41 +0200

    Changes for running sim

--- src/main/java/pl/edu/agh/cs/kraksim/KraksimRunner.java
+++ src/main/java/pl/edu/agh/cs/kraksim/KraksimRunner.java
@@ -14,85 +14,187 @@ import java.util.Properties;
 
 public class KraksimRunner {
 	public static final Logger LOGGER = Logger.getLogger(KraksimRunner.class);
-	
+
+	private static final boolean testTries = false;
+
+	private static final String[] configFiles = new String[] {"low", "mid", "hig"};
 	/**
 	 * Main
 	 *
 	 * @param args may contain config file path
 	 */
 	public static void main(String[] args) {
-		for (int i = 0; i<50 ; i++) {
-			if (args.length > 0) {
-				KraksimConfigurator.setConfigPath(args[0]);
-			}
 
-			final Properties props = KraksimConfigurator.getPropertiesFromFile();
+		if(testTries)
+		{
+			String startName = configFiles[0];
 
-			props.setProperty("statOutFile", props.getProperty("statOutFile") + Integer.toString (i + 1));
-			// we assume that if there is no word about visualisation in config,
-			// then it is necessary...
-			// but if there is...
-			boolean visualise = !(props.containsKey("visualization") && props.getProperty("visualization").equals("false"));
-
-//			boolean visualise = false;
-
-			// set up Logger
-			PropertyConfigurator.configure("src\\main\\resources\\log4j.properties");
-
-
-			// set up the prediction
-			String predictionEnabled = props.getProperty("enablePrediction");
-			String predictionFileConfig = props.getProperty("predictionFile");
-			if (!"true".equals(predictionEnabled)) {
-				KraksimConfigurator.disablePrediction();
-				LOGGER.info("Prediction disabled");
-			} else {
-				KraksimConfigurator.configurePrediction(predictionFileConfig);
-				LOGGER.info("Prediction configured with file: " + predictionFileConfig);
-			}
-
-			// start simulation - with or without visualisation
-			if (visualise) {
-				String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
-				try {
-					UIManager.setLookAndFeel(lookAndFeel);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-
-				javax.swing.SwingUtilities.invokeLater(new Runnable() {
-					public void run() {
-						JFrame frame = new JFrame("Kraksim Visualiser");
-						frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-						frame.getContentPane().add(new MainVisualisationPanel(props));
-						frame.setSize(800, 600);
-						frame.setVisible(true);
-						Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-						int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
-						int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
-						frame.setLocation(x, y);
+			for (String subName : configFiles){
+				for (int i = 0; i<150 ; i++) {
+					if (args.length > 0) {
+						KraksimConfigurator.setConfigPath(args[0]);
 					}
-				});
-			} else {
 
-				KmeansClustering.setProperties(props);
+					System.out.println("Current try: " + Integer.toString(i) + " for: " + subName);
 
-				Simulation sim = new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props));
+					final Properties props = KraksimConfigurator.getPropertiesFromFile();
 
-				Thread runner = new Thread(sim);
-				runner.start();
+					String traffic_file = props.getProperty("travelSchemeFile");
+					traffic_file = traffic_file.replaceAll(startName, subName);
+
+					props.setProperty("travelSchemeFile", traffic_file);
+
+					String statOutFileName = props.getProperty("statOutFile") + Integer.toString(i + 1);
+
+					statOutFileName = statOutFileName.replaceAll(startName, subName);
+
+					props.setProperty("statOutFile", statOutFileName);
+					// we assume that if there is no word about visualisation in config,
+					// then it is necessary...
+					// but if there is...
+					boolean visualise = !(props.containsKey("visualization") && props.getProperty("visualization").equals("false"));
+
+					//			boolean visualise = false;
+
+					// set up Logger
+					PropertyConfigurator.configure("src\\main\\resources\\log4j.properties");
 
 
-//				KmeansClustering.setProperties(props);
-//				Thread simThread = new Thread(new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props)));
+					// set up the prediction
+					String predictionEnabled = props.getProperty("enablePrediction");
+					String predictionFileConfig = props.getProperty("predictionFile");
+					if (!"true".equals(predictionEnabled)) {
+						KraksimConfigurator.disablePrediction();
+						LOGGER.info("Prediction disabled");
+					} else {
+						KraksimConfigurator.configurePrediction(predictionFileConfig);
+						LOGGER.info("Prediction configured with file: " + predictionFileConfig);
+					}
 
-//				simThread.start();
-				try {
-					runner.join();
-				} catch (InterruptedException e) {
-					LOGGER.error("InterruptedException", e);
+					// start simulation - with or without visualisation
+					if (visualise) {
+						String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+						try {
+							UIManager.setLookAndFeel(lookAndFeel);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+
+						javax.swing.SwingUtilities.invokeLater(new Runnable() {
+							public void run() {
+								JFrame frame = new JFrame("Kraksim Visualiser");
+								frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+								frame.getContentPane().add(new MainVisualisationPanel(props));
+								frame.setSize(800, 600);
+								frame.setVisible(true);
+								Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+								int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+								int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+								frame.setLocation(x, y);
+							}
+						});
+					} else {
+
+						KmeansClustering.setProperties(props);
+
+						Simulation sim = new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props));
+
+						Thread runner = new Thread(sim);
+						runner.start();
+
+
+						//				KmeansClustering.setProperties(props);
+						//				Thread simThread = new Thread(new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props)));
+
+						//				simThread.start();
+						try {
+							runner.join();
+						} catch (InterruptedException e) {
+							LOGGER.error("InterruptedException", e);
+						}
+					}
 				}
+			}
+		}
+		else
+		{
+			runProgram(args);
+		}
+	}
+
+	private static void runProgram(String[] args)
+	{
+		if (args.length > 0) {
+			KraksimConfigurator.setConfigPath(args[0]);
+		}
+
+		final Properties props = KraksimConfigurator.getPropertiesFromFile();
+
+		props.setProperty("statOutFile", props.getProperty("statOutFile"));
+		// we assume that if there is no word about visualisation in config,
+		// then it is necessary...
+		// but if there is...
+		boolean visualise = !(props.containsKey("visualization") && props.getProperty("visualization").equals("false"));
+
+		//			boolean visualise = false;
+
+		// set up Logger
+		PropertyConfigurator.configure("src\\main\\resources\\log4j.properties");
+
+
+		// set up the prediction
+		String predictionEnabled = props.getProperty("enablePrediction");
+		String predictionFileConfig = props.getProperty("predictionFile");
+		if (!"true".equals(predictionEnabled)) {
+			KraksimConfigurator.disablePrediction();
+			LOGGER.info("Prediction disabled");
+		} else {
+			KraksimConfigurator.configurePrediction(predictionFileConfig);
+			LOGGER.info("Prediction configured with file: " + predictionFileConfig);
+		}
+
+		// start simulation - with or without visualisation
+		if (visualise) {
+			String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
+			try {
+				UIManager.setLookAndFeel(lookAndFeel);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					JFrame frame = new JFrame("Kraksim Visualiser");
+					frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+					frame.getContentPane().add(new MainVisualisationPanel(props));
+					frame.setSize(800, 600);
+					frame.setVisible(true);
+					Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+					int x = (int) ((dimension.getWidth() - frame.getWidth()) / 2);
+					int y = (int) ((dimension.getHeight() - frame.getHeight()) / 2);
+					frame.setLocation(x, y);
+				}
+			});
+		} else {
+
+			KmeansClustering.setProperties(props);
+
+			Simulation sim = new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props));
+
+			Thread runner = new Thread(sim);
+			runner.start();
+
+
+			//				KmeansClustering.setProperties(props);
+			//				Thread simThread = new Thread(new Simulation(KraksimConfigurator.prepareInputParametersForSimulation(props)));
+
+			//				simThread.start();
+			try {
+				runner.join();
+			} catch (InterruptedException e) {
+				LOGGER.error("InterruptedException", e);
 			}
 		}
 	}
--- src/main/java/pl/edu/agh/cs/kraksim/main/drivers/DriverArchetype.java
+++ src/main/java/pl/edu/agh/cs/kraksim/main/drivers/DriverArchetype.java
@@ -10,26 +10,42 @@ public class DriverArchetype {
 
     public DriverArchetype()
     {
-        float rand = new Random().nextFloat();
-        if (rand <= 0.25f)
+        Random r = new Random();
+        float rand = r.nextFloat();
+        if (rand <= 0.1f) // agresywny kierowca
         {
             this.aggression = 1.0f;
             this.fear = 0.0f;
+            this.tiredness = 0.0f;
+            this.independence = 1.0f;
         }
-        else if (rand <= 0.5f)
-        {
-            this.fear = 1.0f;
-            this.aggression = 0.0f;
-        }
-        else if(rand <= 0.75f)
+        else if (rand <= 0.3f) // zmęczony kierowca
         {
             this.fear = 0.5f;
             this.aggression = 0.1f;
+            this.tiredness = 1.0f;
+            this.independence = 1.0f;
         }
-        else
+        else if(rand <= 0.7f) // normalny kierowca
         {
             this.fear = 0.25f;
             this.aggression = 0.3f;
+            this.tiredness = 0.0f;
+            this.independence = 0.5f;
+        }
+        else if(rand <= 0.75) // niedzielny kierowca
+        {
+            this.fear = 1.0f;
+            this.aggression = 0.0f;
+            this.tiredness = 0.0f;
+            this.independence = 0.1f;
+        }
+        else // losowy
+        {
+            this.fear = r.nextFloat();
+            this.aggression = r.nextFloat();
+            this.tiredness = r.nextFloat();
+            this.independence = r.nextFloat();
         }
 
 //        this.aggression = 1.0f;
