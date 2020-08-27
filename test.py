f9f9cc11abaa35d8c4453d3db3524438dc65d6cc
Pysiokot <bszczypka@nano-games.com>
2020-08-27 14:11:35 +0200

    Changed logic for calculating AVG vel

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from xml.dom import minidom as xd
 from os import listdir
 from os.path import isfile, join
 import os
+import math
 
 def getCalculatedResult(files_res, items_count):
     result = {}
@@ -23,7 +24,7 @@ def addNewResult(data_holder, key_name, data):
 
     data_holder[key_name] = curr_value
 
-N = 150
+N = 50
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 mypath = os.path.join(dirname, 'output')
@@ -51,7 +52,8 @@ for onlyfile in onlyfiles:
     xmldoc = xd.parse(os.path.join(mypath, onlyfile))
     itemlist = xmldoc.getElementsByTagName('period')
 
-    item_ctr = 0
+    av_item_ctr = 0
+    arv_item_ctr = 0
     zeros = 0
     mean_av, mean_arv = 0.0, 0.0
 
@@ -63,23 +65,33 @@ for onlyfile in onlyfiles:
         if attr_val == 900: # pomijamy kilka pierwszych wyników (rozgrzewka)
 
             if zeros != 0 and zeros != 2 and zeros != 3 and zeros != 6:
-                file_res.append((mean_av/item_ctr, mean_arv/item_ctr))
+                file_res.append((mean_av/av_item_ctr, mean_arv/arv_item_ctr))
 
             zeros += 1
                 
             mean_av, mean_arv = 0.0, 0.0
 
-            item_ctr = 0
+            av_item_ctr = 0
+            arv_item_ctr = 0
         elif attr_val < 900:
             continue
 
         if zeros == 2 or zeros == 3 or zeros == 6:
             continue
 
-        item_ctr += 1
 
-        mean_av += float(item.attributes['avg_velocity'].value.replace(',', '.'))
-        mean_arv += float(item.attributes['avg_riding_velocity'].value.replace(',', '.'))
+# if NAN -> pominac?
+        av_vel = float(item.attributes['avg_velocity'].value.replace(',', '.'))
+        av_r_vel = float(item.attributes['avg_riding_velocity'].value.replace(',', '.'))
+        if not math.isnan(av_vel):
+            av_item_ctr += 1
+            mean_av += av_vel
+
+        if not math.isnan(av_r_vel):
+            arv_item_ctr += 1
+            mean_arv += av_r_vel
+            
+        
         
         # print(item.attributes['begin'].value)
 
